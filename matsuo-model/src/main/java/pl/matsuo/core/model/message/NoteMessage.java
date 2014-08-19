d8d32c5cc81931a61ee83890678668f7f3f3b162
Marek Romanowski <marek.romanowski@gmail.com>
2014-08-19 09:45:44 +0200

    Add note fields.

--- matsuo-model/src/main/java/pl/matsuo/core/model/message/NoteMessage.java
+++ matsuo-model/src/main/java/pl/matsuo/core/model/message/NoteMessage.java
@@ -2,11 +2,31 @@ package pl.matsuo.core.model.message;
 
 import javax.persistence.Entity;
 
+import static pl.matsuo.core.model.message.NoteStatus.*;
+
 
 /**
  * Created by marek on 07.07.14.
  */
 @Entity
 public class NoteMessage extends AbstractMessage {
+
+
+  private NotePriority priority;
+  private NoteStatus status = OPEN;
+
+
+  public NotePriority getPriority() {
+    return priority;
+  }
+  public void setPriority(NotePriority priority) {
+    this.priority = priority;
+  }
+  public NoteStatus getStatus() {
+    return status;
+  }
+  public void setStatus(NoteStatus status) {
+    this.status = status;
+  }
 }
 
---
+++ matsuo-model/src/main/java/pl/matsuo/core/model/message/NotePriority.java
@@ -0,0 +1,14 @@
+package pl.matsuo.core.model.message;
+
+
+/**
+ * Created by marek on 19.08.14.
+ */
+public enum NotePriority {
+
+  IMPORTANT,
+  WARNING,
+  INFO
+
+}
+
