9e531bccc5120f777b63cddb90010a370ff5121a
Marek Romanowski <marek.romanowski@gmail.com>
2014-08-19 11:57:48 +0200

    Add statuses to note messages.

--- matsuo-web/src/main/java/pl/matsuo/core/web/controller/message/NoteMessageController.java
+++ matsuo-web/src/main/java/pl/matsuo/core/web/controller/message/NoteMessageController.java
@@ -1,8 +1,16 @@
 package pl.matsuo.core.web.controller.message;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.matsuo.core.model.message.NoteMessage;
+import pl.matsuo.core.model.message.NoteStatus;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static pl.matsuo.core.model.message.NoteStatus.*;
 
 
 /**
@@ -16,6 +24,35 @@ public class NoteMessageController extends AbstractMessageController<NoteMessage
   @Override
   protected NoteMessage copyMessage(NoteMessage message) {
     return message;
+  }
+
+
+  protected HttpEntity<String> changeAppointmentStatus(Integer id, NoteStatus status) {
+    NoteMessage appointment = database.findById(NoteMessage.class, id);
+    appointment.setStatus(status);
+    database.update(appointment);
+    return new HttpEntity<String>(appointment.getStatus().name());
+  }
+
+
+  @RequestMapping(value = "/{id}/close", method = POST)
+  @ResponseStatus(CREATED)
+  public HttpEntity<String> close(@PathVariable("id") Integer id) {
+    return changeAppointmentStatus(id, CLOSED);
+  }
+
+
+  @RequestMapping(value = "/{id}/cancel", method = POST)
+  @ResponseStatus(CREATED)
+  public HttpEntity<String> cancel(@PathVariable("id") Integer id) {
+    return changeAppointmentStatus(id, CANCELLED);
+  }
+
+
+  @RequestMapping(value = "/{id}/open", method = POST)
+  @ResponseStatus(CREATED)
+  public HttpEntity<String> open(@PathVariable("id") Integer id) {
+    return changeAppointmentStatus(id, OPEN);
   }
 }
 
