9f369e280c59e440df472f58b02b1d97aa2d4aa8
Marek Romanowski <marek.romanowski@gmail.com>
2015-02-03 19:54:29 +0100

    Add AccessLogFilter test

---
+++ matsuo-web/src/test/java/pl/matsuo/core/web/filter/TestAccessLogFilter.java
@@ -0,0 +1,50 @@
+package pl.matsuo.core.web.filter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import pl.matsuo.core.model.log.AccessLog;
+import pl.matsuo.core.service.db.Database;
+import pl.matsuo.core.service.permission.PermissionService;
+import pl.matsuo.core.test.data.TestSessionState;
+
+import javax.servlet.FilterChain;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestAccessLogFilter {
+
+
+  @Mock
+  Database database;
+  @Mock
+  PermissionService permissionService;
+  @Mock
+  TestSessionState sessionState;
+  @Mock
+  FilterChain chain;
+  @InjectMocks
+  AccessLogFilter accessLogFilter = new AccessLogFilter();
+
+
+  @Test
+  public void testDoFilter() throws Exception {
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    MockHttpServletResponse response = new MockHttpServletResponse();
+
+    accessLogFilter.doFilter(request, response, chain);
+
+    verify(database).create(any(AccessLog.class));
+    verify(chain).doFilter(request, response);
+  }
+}
+
