7b5d80516c6c362ef4d9598bf9943cbe36006a7c
Marek Romanowski <marek.romanowski@gmail.com>
2015-02-11 22:38:19 +0100

    Comparator util.

---
+++ matsuo-util/src/main/java/pl/matsuo/core/util/ComparatorUtil.java
@@ -0,0 +1,35 @@
+package pl.matsuo.core.util;
+
+import java.util.Comparator;
+import java.util.function.Function;
+
+/**
+ * Created by marek on 11.02.15.
+ */
+public class ComparatorUtil {
+
+
+  public static <T> Comparator<T> comparator(Function<T, Comparable> ... getters) {
+
+
+    return (o1, o2) -> {
+      for (Function<T, Comparable> getter : getters) {
+        Comparable o1Result = getter.apply(o1);
+        Comparable o2Result = getter.apply(o2);
+
+        if (o1Result == null && o2Result == null) {
+          // do nothin'
+        } else if (o1Result == null || o2Result == null) {
+          return o1Result == null ? -1 : 1;
+        } else {
+          int compareResult = o1Result.compareTo(o2Result);
+          if (compareResult != 0) {
+            return compareResult;
+          }
+        }
+      }
+
+      return 0;
+    };
+  }
+}
---
+++ matsuo-util/src/test/java/pl/matsuo/core/util/TestComparatorUtil.java
@@ -0,0 +1,67 @@
+package pl.matsuo.core.util;
+
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+
+import static java.util.Arrays.*;
+import static org.junit.Assert.*;
+import static pl.matsuo.core.util.ComparatorUtil.*;
+import static pl.matsuo.core.util.DateUtil.*;
+
+public class TestComparatorUtil {
+
+
+  private class X {
+
+
+    final Integer integer;
+    final String string;
+    final Date date;
+
+
+    private X(Integer integer, String string, Date date) {
+      this.integer = integer;
+      this.string = string;
+      this.date = date;
+    }
+
+
+    public Integer getInteger() {
+      return integer;
+    }
+    public String getString() {
+      return string;
+    }
+    public Date getDate() {
+      return date;
+    }
+  }
+
+
+  public List<X> list() {
+    return asList(
+        new X(null, null, null),
+        new X(3, "c", date(2015, 0, 3)),
+        new X(2, "b", date(2015, 0, 2)),
+        new X(1, "a", date(2015, 0, 1)),
+        new X(null, null, null));
+  }
+
+
+  @Test
+  public void testComparator() throws Exception {
+    asList(comparator(X::getInteger), comparator(X::getDate), comparator(X::getString)).forEach(comparator -> {
+      List<X> list = list();
+      list.sort(comparator);
+
+      assertNull(list.get(0).getInteger());
+      assertNull(list.get(1).getInteger());
+      assertEquals((Integer) 1, list.get(2).getInteger());
+      assertEquals((Integer) 2, list.get(3).getInteger());
+      assertEquals((Integer) 3, list.get(4).getInteger());
+    });
+  }
+}
+
