c5f6330c66091ccac683f3e6cbff653f6419e24a
Marek Romanowski <marek.romanowski@gmail.com>
2015-10-20 23:23:29 +0200

    Multi file aggregation

--- add_comments.sh
+++ add_comments.sh
@@ -7,9 +7,9 @@ HOST=http://gitlab.matsuo.pl
 PROJECT_ID=$(curl -H "PRIVATE-TOKEN: $USER_PRIVATE_TOKEN" $HOST/ci/api/v1/projects | python -m json.tool | grep -E "gitlab_id|\"id\"" | grep "id\": $CI_PROJECT_ID," -B 1 | grep "\"gitlab_id\"" | cut -d ":" -f 2 | cut -d "," -f 1 | sed -e 's/^[[:space:]]*//')
 COMMIT_ID=$CI_BUILD_REF
 
-CHECKSTYLE=$(cat target/checkstyle-result.xml  | grep "<error " | wc -l)
-PMD=$(cat target/pmd.xml | grep "<violation" | wc -l)
-FIND_BUGS=$(cat target/findbugsXml.xml | sed 's/total_bugs/\ntotal_bugs/g' | cut -f 1 -d " " | grep total_bugs | cut -f 2 -d "'" | head -n 1)
+CHECKSTYLE=$(for i in `find -name checkstyle-result.xml`; do cat $i; done | grep "<error " | wc -l)
+PMD=$(for i in `find -name pmd.xml`; do cat $i; done | grep "<violation" | wc -l)
+FIND_BUGS=$(for i in `find -name findbugsXml.xml`; do cat $i | tr "<" "\n" | grep "^FindBugsSummary" | tr " " "\n" | grep total_bugs | cut -d "'" -f 2; done | awk '{s+=$1} END {print s}')
 
 
 URL=$HOST/api/v3/projects/$PROJECT_ID/repository/commits/$COMMIT_ID/comments
@@ -36,4 +36,4 @@ done
 
 curl --data $"note=$NOTE" \
     -H "PRIVATE-TOKEN: $USER_PRIVATE_TOKEN" \
-    $URL 
+    $URL
